62bc934c8012a161f6f0d26b88e273ee9fa2671b
Mauro Fernanadez <mauro_9_2@hotmail.com>
2016-03-03 13:00:38 -0500

    clase2 jdbc

---
+++ Clase02/EurekaCs/src/eurekacs/EurekaCs.java
@@ -0,0 +1,19 @@
+
+package eurekacs;
+
+import pe.mauro.eurekacs.view.LogonView;
+
+/**
+ *
+ * @author Alumno
+ */
+public class EurekaCs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LogonView.main(null);
+    }
+    
+}
